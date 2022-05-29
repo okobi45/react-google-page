32f3d295d423c89bdfaf24a70c4f0416bdb5e864
okobi45 <okobieigbe@gmail.com>
2022-05-29 21:01:34 +0100

    added Navbar to component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import Home from "./component/Home";
 import Error from "./component/Error";
 import {Routes, Route} from 'react-router-dom'
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="*" element={<Error/>}/>
